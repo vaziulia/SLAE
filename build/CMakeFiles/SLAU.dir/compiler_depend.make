4c5ac984f967a45cae7c5e72c22a23c7d99d76d2
Vasilisa <vaziulia.vsphystech.edu>
2023-03-05 00:20:44 +0300

    add cmake

--- build/CMakeFiles/SLAU.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for SLAU.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vasilisa/c++/SLAU
-# Build directory: /home/vasilisa/c++/SLAU/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
